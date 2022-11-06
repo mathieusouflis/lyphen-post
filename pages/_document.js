f763c9925b226b69d323c927c3abf5738ae398bd
Mathieu Souflis <mathieu@souflis.fr>
2022-11-06 22:27:16 +0100

    ajout du _document

---
+++ pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html className='dark'>
+      <Head />
+      <body className='bg-zinc-200 dark:bg-zinc-700'>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
