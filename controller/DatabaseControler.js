113eaef1f9255c7f4d56faf0baf6823f455dbc9f
Mathieu Souflis <mathieu@souflis.fr>
2022-11-09 16:25:50 +0100

    Databasecontroler update

--- controller/DatabaseControler.js
+++ controller/DatabaseControler.js
@@ -1,40 +1,59 @@
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 
-const MONGODB_URI = process.env.MONGODB_URI
+// const MONGODB_URI = process.env.MONGODB_URI
 
-if (!MONGODB_URI) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  )
-}
+// if (!MONGODB_URI) {
+//   throw new Error(
+//     'Please define the MONGODB_URI environment variable inside .env.local'
+//   )
+// }
 
-let cached = global.mongoose
+// let cached = global.mongoose
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null }
-}
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null }
+// }
 
 
+// class DatabaseControler {
+//   static async connect() {
+//     if (cached.conn) {
+//       return cached.conn
+//     }
+
+//     if (!cached.promise) {
+//       const opts = {
+//         useUnifiedTopology: true,
+//         useNewUrlParser: true
+//       }
+
+//       cached.promise = mongoose.connect(MONGODB_URI, opts).then(mongoose => {
+//         return mongoose
+//       })
+//     }
+//     cached.conn = await cached.promise
+//     return cached.conn
+//   }
+// }
+
+// module.exports = { DatabaseControler }
+
+
+import mongoose from 'mongoose'
+
+const connection = {} /* creating connection object*/
 class DatabaseControler {
-  static async connect() {
-    if (cached.conn) {
-      return cached.conn
+  static connect = async () => {
+    /* check if we have connection to our databse*/
+    if (connection.isConnected) {
+      return
     }
 
-    if (!cached.promise) {
-      const opts = {
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-        useNewUrlParser: true
-      }
+    /* connecting to our database */
+    const db = mongoose.connect(process.env.MONGODB_URI)
 
-      cached.promise = mongoose.connect(MONGODB_URI, opts).then(mongoose => {
-        return mongoose
-      })
-    }
-    cached.conn = await cached.promise
-    return cached.conn
+    connection.isConnected = db.connections[0].readyState
+
   }
 }
 
--- pages/api/login/index.js
+++ pages/api/login/index.js
@@ -1,8 +1,9 @@
 //UTILS IMPORT
 const { loginUserValidator } = require("../../../lib/validators/loginUserValidator")
-
+const { DatabaseControler } = require("../../../controller/DatabaseControler")
 const login = async (req, res) => {
   const data = req.body
+  await DatabaseControler.connect()
   await loginUserValidator(data.email, data.password) === true ? res.status(200).json({ message: "Login Ok", status: 200 }) : res.status(401).json({ message: "Login Failed", status: 401 })
   res.end()
 }
