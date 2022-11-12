851bb25dc6c138ca85f481da36b6d1854beb6918
Mathieu Souflis <mathieu@souflis.fr>
2022-11-12 01:51:04 +0100

    azd

--- pages/login/index.js
+++ pages/login/index.js
@@ -14,7 +14,7 @@ const Login = () => {
 
   const submitLogin = async (e) => {
     e.preventDefault()
-    const status = await signIn('credentials', {
+    const status = await signIn('Credentials', {
       redirect: false,
       email,
       password,
