dbfc4d23ea0acbb64a09b1db111454bedd37ae67
Mathieu Souflis <mathieu@souflis.fr>
2022-11-25 17:21:09 +0100

    Beautify The API

--- pages/api/post/like.js
+++ pages/api/post/like.js
@@ -1,13 +1,26 @@
+//CONTROLERS IMPORTS
 const { PostControler } = require('/controller/PostControler')
 const { DatabaseControler } = require('/controller/DatabaseControler')
+
+//VALIDATORS IMPORTS
 const { apiValidator } = require("/lib/validators/apiValidator.js")
 
 const likePost = async (req, res) => {
+
   const data = JSON.parse(req.body)
   apiValidator(data.apiKey, res)
+
   DatabaseControler.connect()
-  await PostControler.like(data.userId, data.postId) === true ? res.json({ message: "Like ajouté !", status: 200 }) : res.json({ message: "Like retiré !", status: 200 })
+
+  await PostControler.like(data.userId, data.postId) === true ? res.json({
+    message: "Like ajouté !",
+    status: 200
+  }) : res.json({
+    message: "Like retiré !",
+    status: 200
+  })
   res.end()
+
 }
 
 export default likePost
--- pages/api/user/create.js
+++ pages/api/user/create.js
@@ -1,25 +1,50 @@
+//LIB IMPORTS
+const { hash } = require("bcryptjs")
+
 //CONTROLERS IMPORTS
 const { UserControler } = require('/controller/UserControler')
 const { DatabaseControler } = require('/controller/DatabaseControler')
+
+//VALIDATORS IMPORTS
 const { usernameValidator } = require('/lib/validators/usernameValidator')
 const { emailValidator } = require('/lib/validators/emailValidator')
 const { passwordValidator } = require('/lib/validators/passwordValidator')
-const { hash } = require("bcryptjs")
 const { apiValidator } = require("/lib/validators/apiValidator.js")
 
 const createUser = async (req, res) => {
   const data = JSON.parse(req.body)
   apiValidator(data.apiKey, res)
+
   DatabaseControler.connect()
+
   const usernameValidity = await usernameValidator(data.username)
   const emailValidity = await emailValidator(data.email)
   const passwordValidity = await passwordValidator(data.password)
-  if (!usernameValidity || !emailValidity || !passwordValidity) {
-    res.status(401).json({ message: "Some errors", status: 401, usernameValidity, emailValidity, passwordValidity }).end()
-  } else {
+
+  if (usernameValidity && emailValidity && passwordValidity) {
+
     UserControler.create(data.username, data.email, await hash(data.password, 12))
-    res.status(200).json({ message: "User Added", status: 200, usernameValidity, emailValidity, passwordValidity }).end()
+
+    res.status(200).json({
+      message: "User Added",
+      status: 200,
+      usernameValidity,
+      emailValidity,
+      passwordValidity
+    }).end()
+
+  } else {
+
+    res.status(401).json({
+      message: "Some errors",
+      status: 401,
+      usernameValidity,
+      emailValidity,
+      passwordValidity
+    }).end()
+
   }
+
 }
 
 export default createUser
